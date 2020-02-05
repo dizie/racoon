26cab4c40e5b13f183ea6006a1d7b5e937a74c58
Sam Smith <xksmitty@hotmail.com>
2020-02-05 18:08:11 +1100

    added logging

--- monitor.py
+++ monitor.py
@@ -1,8 +1,27 @@
 from scrape import scraper
 import time
+import logging
 
 URL = 'https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/85320e2ea5424dfaaa75ae62e5c06e61'
 DEBUG = False
+
+LOG_LEVEL = logging.INFO
+
+if DEBUG is True:
+    LOG_LEVEL = logging.DEBUG
+
+LOG_PATH = '/tmp/racoon.log'
+
+logging.basicConfig(format='%(asctime)s.%(msecs)03d - %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename=LOG_PATH,
+                    level=LOG_LEVEL)
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s.%(msecs)03d - %(levelname)s - %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+logger = logging.getLogger(__name__)
 
 
 def main():
@@ -15,7 +34,7 @@ def main():
             print("Going for another run")
 
         results = scraper(URL, DEBUG)
-        print(results)
+        logger.info(results)
         r_cases = results["Total Confirmed"].replace(',', '')
         r_casualties = int(results["Total Deaths"].replace(',', ''))
         r_recoveries = results["Total Recovered"].replace(',', '')
@@ -29,31 +48,31 @@ def main():
             if cases < r_cases:
                 diff = r_cases - cases
                 cases = r_cases
-                print("Confirmed cases have risen by: {}. Count now stands at: {}".format(diff, cases))
+                logger.info("Confirmed cases have risen by: {}. Count now stands at: {}".format(diff, cases))
             elif cases > r_cases:
                 diff = cases - r_cases
                 cases = r_cases
-                print("Confirmed cases have decreased by: {}. Count now stands at: {}".format(diff, cases))
+                logger.info("Confirmed cases have decreased by: {}. Count now stands at: {}".format(diff, cases))
 
         if casualties != r_casualties:
             if casualties < r_casualties:
                 diff = r_casualties - casualties
                 casualties = r_casualties
-                print("Fatal cases have risen by: {}. Count now stands at: {}".format(diff, casualties))
+                logger.info("Fatal cases have risen by: {}. Count now stands at: {}".format(diff, casualties))
             elif casualties > r_casualties:
                 diff = casualties - r_casualties
                 casualties = r_casualties
-                print("Fatal cases have decreased by: {}. Count now stands at: {}".format(diff, casualties))
+                logger.info("Fatal cases have decreased by: {}. Count now stands at: {}".format(diff, casualties))
 
         if recoveries != r_recoveries:
             if recoveries < r_recoveries:
                 diff = r_recoveries - recoveries
                 recoveries = r_recoveries
-                print("Recovery cases have risen by: {}. Count now stands at: {}".format(diff, recoveries))
+                logger.info("Recovery cases have risen by: {}. Count now stands at: {}".format(diff, recoveries))
             if recoveries > r_recoveries:
                 diff = recoveries - r_recoveries
                 recoveries = r_recoveries
-                print("Recovery cases have decreased by: {}. Count now stands at: {}".format(diff, recoveries))
+                logger.info("Recovery cases have decreased by: {}. Count now stands at: {}".format(diff, recoveries))
 
         time.sleep(60)
 
