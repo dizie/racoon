9c6a5741a2b278ad9d95b23f21d3051bf4b02aed
Sam Smith <xksmitty@hotmail.com>
2020-02-06 16:23:46 +1100

    additional error catching

--- monitor.py
+++ monitor.py
@@ -39,44 +39,48 @@ def main():
         if results is False:
             logger.error("An error occurred retrieving stats from {}. Trying again in {}".format(URL, WAIT_TIME))
         else:
-            r_cases = int(results["Total Confirmed"].replace(',', ''))
-            r_casualties = int(results["Total Deaths"].replace(',', ''))
-            r_recoveries = int(results["Total Recovered"].replace(',', ''))
+            try:
+                r_cases = int(results["Total Confirmed"].replace(',', ''))
+                r_casualties = int(results["Total Deaths"].replace(',', ''))
+                r_recoveries = int(results["Total Recovered"].replace(',', ''))
 
-            if cases is None:
-                cases = r_cases
-                casualties = r_casualties
-                recoveries = r_recoveries
-
-            if cases != r_cases:
-                if cases < r_cases:
-                    diff = r_cases - cases
+                if cases is None:
                     cases = r_cases
-                    logger.info("Confirmed cases have risen by: {}. Count now stands at: {}".format(diff, cases))
-                elif cases > r_cases:
-                    diff = cases - r_cases
-                    cases = r_cases
-                    logger.info("Confirmed cases have decreased by: {}. Count now stands at: {}".format(diff, cases))
-
-            if casualties != r_casualties:
-                if casualties < r_casualties:
-                    diff = r_casualties - casualties
                     casualties = r_casualties
-                    logger.info("Fatal cases have risen by: {}. Count now stands at: {}".format(diff, casualties))
-                elif casualties > r_casualties:
-                    diff = casualties - r_casualties
-                    casualties = r_casualties
-                    logger.info("Fatal cases have decreased by: {}. Count now stands at: {}".format(diff, casualties))
+                    recoveries = r_recoveries
 
-            if recoveries != r_recoveries:
-                if recoveries < r_recoveries:
-                    diff = r_recoveries - recoveries
-                    recoveries = r_recoveries
-                    logger.info("Recovery cases have risen by: {}. Count now stands at: {}".format(diff, recoveries))
-                if recoveries > r_recoveries:
-                    diff = recoveries - r_recoveries
-                    recoveries = r_recoveries
-                    logger.info("Recovery cases have decreased by: {}. Count now stands at: {}".format(diff, recoveries))
+                if cases != r_cases:
+                    if cases < r_cases:
+                        diff = r_cases - cases
+                        cases = r_cases
+                        logger.info("Confirmed cases have risen by: {}. Count now stands at: {}".format(diff, cases))
+                    elif cases > r_cases:
+                        diff = cases - r_cases
+                        cases = r_cases
+                        logger.info("Confirmed cases have decreased by: {}. Count now stands at: {}".format(diff, cases))
+
+                if casualties != r_casualties:
+                    if casualties < r_casualties:
+                        diff = r_casualties - casualties
+                        casualties = r_casualties
+                        logger.info("Fatal cases have risen by: {}. Count now stands at: {}".format(diff, casualties))
+                    elif casualties > r_casualties:
+                        diff = casualties - r_casualties
+                        casualties = r_casualties
+                        logger.info("Fatal cases have decreased by: {}. Count now stands at: {}".format(diff, casualties))
+
+                if recoveries != r_recoveries:
+                    if recoveries < r_recoveries:
+                        diff = r_recoveries - recoveries
+                        recoveries = r_recoveries
+                        logger.info("Recovery cases have risen by: {}. Count now stands at: {}".format(diff, recoveries))
+                    if recoveries > r_recoveries:
+                        diff = recoveries - r_recoveries
+                        recoveries = r_recoveries
+                        logger.info("Recovery cases have decreased by: {}. Count now stands at: {}".format(diff, recoveries))
+            except KeyError:
+                logger.error("Key error in data. Trying again in {}. Data: {}".format(WAIT_TIME, results))
+                pass
 
         time.sleep(WAIT_TIME)
 
