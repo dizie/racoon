e3df0046b11e8dfbe560b3024e30fb6406686e36
Sam Smith <xksmitty@hotmail.com>
2020-03-30 13:08:03 +1100

    add scraper functions for country stats script

---
+++ country_stats.py
@@ -0,0 +1,25 @@
+from scrape2 import country_data
+
+DEBUG = False
+
+
+def get_value(values):
+    population = int(values["pop"].replace(',', ''))
+    cases = int(values["cases"].replace(',', ''))
+    values["percent"] = "{0:.2f}%".format(cases / population * 100, 2)
+
+    return values
+
+
+def main():
+    countries = ["Australia", "Canada", "New Zealand", "US"]
+
+    for c in countries:
+        stats = country_data(c)
+        stats = get_value(stats)
+
+        print(c, "=",  stats["cases"], "/", stats["pop"], "({})".format(stats["percent"]))
+
+
+if __name__ == "__main__":
+    main()
